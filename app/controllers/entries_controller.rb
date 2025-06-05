7366435911a7cb337cd3da4a3307d2b0ef4c57e5
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-06-05 00:56:26 +0000

    added images

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,10 +5,12 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new
+    @entry.user_id = session["user_id"]
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry.uploaded_image.attach(params[:uploaded_image])
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
